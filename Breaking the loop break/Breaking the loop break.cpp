221f61732e0062eb52da94edffae4eb51448fc51
WaiperOK <ruslan.topol2@gmail.com>
2022-07-02 19:19:50 +0300

    Добавьте файлы проекта.

---
+++ Breaking the loop break/Breaking the loop break.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+
+	cout << " начало цикла " << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << " переменная i = " << i << endl;
+
+		if (i == 5)
+		{
+			break; // прерывание цикла и выход из него
+		}
+	}
+
+	cout << " конец цикла " << endl;
+
+}
